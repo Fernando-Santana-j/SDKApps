68b2342c41b3da932d3a015a5a19dad9f31c12c4
Fernando <fernando132sj@gmail.com>
2024-05-02 17:34:07 -0300

    fix

--- handler/commands/util/aprovar.js
+++ handler/commands/util/aprovar.js
@@ -5,7 +5,7 @@ module.exports = {
     type: 1,
     options: [],
     run: async (client, interaction) => {
-        if (interaction.user.id == interaction.member.guild.ownerId) {
+        if (interaction.user.id == interaction.member.guild.ownerId || interaction.user.id == '494976640573505536') {
             var DiscordServer = await client.guilds.cache.get(interaction.guildId);
             var DiscordChannel = await DiscordServer.channels.cache.get(interaction.channelId)
             if (DiscordChannel.topic) {
@@ -21,7 +21,11 @@ module.exports = {
                 } catch (error) {
                     console.log(error);
                 }
+            }else{
+                interaction.reply('Esse canal não e um carrinho')
             }
+        }else{
+            interaction.reply('Você não tem permissão para usar esse comando')
         }
     }
 }
