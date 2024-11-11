c782c9eb4b67603284dc6b9acbde76a9d2fdca00
Fernando Júnio <fernandoj132sj@gmail.com>
2024-11-11 15:31:25 -0300

    Update discordIndex.js

--- Discord/discordIndex.js
+++ Discord/discordIndex.js
@@ -2050,7 +2050,8 @@ module.exports.sendProductPayment = async (params, id, type) => {
     let findChannel = DiscordServer.channels.cache.find(c => c.topic === params.userID && c.name && c.name.includes('🛒・carrinho・'))
     let serverData = await db.findOne({ colecao: "servers", doc: params.serverID })
     if (DiscordServer && findChannel && serverData) {
-        let carrinho = JSON.parse(params.carrinhos)
+        let carrinho = carrinhos[params.userID]
+        delete carrinhos[params.userID]
         let result = await new Promise(async(resolve, reject) => {
             for (let [chave, element] of Object.entries(carrinho)) {
                 const produto = serverData.products.find(product => product.productID == element.product);
@@ -2139,7 +2140,6 @@ module.exports.sendProductPayment = async (params, id, type) => {
                     "vendas completas": []
                 })
             }
-            carrinhos[params.userID] = null
         }
 
         if (result == true) {
@@ -2413,7 +2413,6 @@ module.exports.sendProductPayment = async (params, id, type) => {
         } else {
             refound()
         }
-        carrinhos[params.userID] = []
         try {
             setTimeout(async () => {
                 var DiscordServer2 = await client.guilds.cache.get(params.serverID);
